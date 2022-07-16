ac8135f736ae6843f7de7277d552801f18a90432
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-16 17:12:49 -0400

    added connection to mailchimp

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
+const { response } = require("express");
 
+require('dotenv').config();
 const app = express();
 
 app.use(express.static("public"));
@@ -29,8 +31,23 @@ app.post("/", (req, res) =>{
         }
       }
     ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = process.env.DB_URL;
+  const options = {
+    method: "POST",
+    auth: process.env.DB_AUTH,
   }
-})
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
